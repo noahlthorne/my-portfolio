2874d4d673553ef1f4ba56c8c118860893ddcc15
Noah Thorne <noahlthorne@gmail.com>
2019-12-10 11:47:48 +0900

    Added background and profile image

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
+import { Grid, Cell } from 'react-mdl';
+import Photo from "../images/P1120527.png"
 
 class Landing extends Component {
   render() {
     return (
-      <div>
-      <h1>Landing</h1>
+      <div style={{width: '100%', margin: 'auto'}}>
+        <Grid className="landing-grid">
+          <Cell col={12}>
+            <img
+              src={Photo} alt="Profile Image" className="profile-photo"
+            />
+          </Cell>
+        </Grid>
       </div>
     );
   }

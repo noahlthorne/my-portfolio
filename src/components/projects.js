e4d9d44e5d9bbad64d250aee56068bd99afea47e
Noah Thorne <noahlthorne@gmail.com>
2019-12-17 14:38:27 +0900

    added some styling and content to projects page

--- src/components/projects.js
+++ src/components/projects.js
@@ -43,13 +43,15 @@ class Projects extends Component {
         </div>
 
         <div className="project-description">
-          <h1>Ronpa</h1>
-          <hr style ={{borderTop: '1px solid #grey', width: '50%'}}/>
-          <h3>Project Overview</h3>
+          <h1>Ronpa!</h1>
+          <hr/>
+          <h4>Project Overview</h4>
           <p>Ronpa is a live debate platform designed to enable structured, live discussion of any issue. Unlike the multitude of text-based discussion platforms online, Ronpa brings people face to face.</p>
-          <h3>Concept</h3>
+          <hr/>
+          <h4>Concept</h4>
           <p>The idea behind Ronpa was to create an approachable, fun way to debate live with strangers or people you know on a topic of your choosing. We wanted to take something that is perceived as intimidating and gamify it, in order to make it something that anyone could engage in, but without the pressure of a typical debate environment.</p>
-          <h3>Challenges and Solutions</h3>
+          <hr/>
+          <h4>Challenges and Solutions</h4>
           <p>The most challening aspect of creating this app was the debate itself. Creating seperate views and unique actions, timers and synchronised browser refreshes depending on your role in the debate is not an easy task. We implemented Action Cable to integrate WebSockets with our application to render the different views for each user and we used JavaScript to render a timer that would update the phase of the debate.</p>
         </div>
 

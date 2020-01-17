926ec3ea018f1a14e179535abc5039dc0348ee79
Noah Thorne <noahlthorne@gmail.com>
2020-01-17 16:39:11 +0900

    attempt to fix routes in production #1

--- src/components/main.js
+++ src/components/main.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { createBrowserHistory as history } from 'history';
 import LandingPage from './landingpage';
 import AboutMe from './aboutme';
 import Contact from './contact';
@@ -7,14 +9,18 @@ import Resume from './resume';
 
 import { Switch, Route } from 'react-router-dom';
 
-const Main = () => (
-    <Switch>
-        <Route exact path ="/" component={LandingPage} />
-        <Route exact path ="/aboutme" component={AboutMe} />
-        <Route exact path ="/contact" component={Contact} />
-        <Route exact path ="/projects" component={Projects} />
-        <Route exact path ="/resume" component={Resume} />
-    </Switch>
-)
+class Main extends React.Component {
+  render() {
+    return (
+      <Switch>
+          <Route exact path ="/" component={LandingPage} />
+          <Route exact path ="/aboutme" component={AboutMe} />
+          <Route exact path ="/contact" component={Contact} />
+          <Route exact path ="/projects" component={Projects} />
+          <Route exact path ="/resume" component={Resume} />
+      </Switch>
+    )
+  }
+}
 
 export default Main;

3436eaaee836a655d6f281ccba68d7d6b0c1b892
Noah Thorne <noahlthorne@gmail.com>
2020-01-17 16:27:26 +0900

    changed routes to exact path

--- src/components/main.js
+++ src/components/main.js
@@ -1,5 +1,4 @@
 import React from 'react';
-
 import LandingPage from './landingpage';
 import AboutMe from './aboutme';
 import Contact from './contact';
@@ -9,13 +8,13 @@ import Resume from './resume';
 import { Switch, Route } from 'react-router-dom';
 
 const Main = () => (
-  <Switch>
-    <Route exact path ="/" component={LandingPage} />
-    <Route path ="/aboutme" component={AboutMe} />
-    <Route path ="/contact" component={Contact} />
-    <Route path ="/projects" component={Projects} />
-    <Route path ="/resume" component={Resume} />
-  </Switch>
+    <Switch>
+        <Route exact path ="/" component={LandingPage} />
+        <Route exact path ="/aboutme" component={AboutMe} />
+        <Route exact path ="/contact" component={Contact} />
+        <Route exact path ="/projects" component={Projects} />
+        <Route exact path ="/resume" component={Resume} />
+    </Switch>
 )
 
 export default Main;

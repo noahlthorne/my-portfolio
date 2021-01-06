658c2d21ae073e19cbefa4990763d1c068d88216
Noah Thorne <48963204+noahlthorne@users.noreply.github.com>
2021-01-06 16:01:45 +0900

    added to skills on resume page

--- src/components/skills.js
+++ src/components/skills.js
@@ -7,7 +7,7 @@ class Skills extends Component {
       <Grid>
         <Cell col={12}>
           <div style={{display: 'flex'}}>
-            <h5>JavaScript | TypeScript| Ruby on Rails | AWS | ReactJS | HTML | CSS | SQL/Postgres | AJAX | Github </h5>
+            <h5 class="skills-list">JavaScript | TypeScript | NodeJS | NestJS | Ruby on Rails | AWS | ReactJS | HTML | CSS | SQL/Postgres | MongoDB</h5>
           </div>
         </Cell>
       </Grid>
